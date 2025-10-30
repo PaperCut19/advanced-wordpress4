acbda17b267aa632e5461f530c5acbc0e3a0d09c
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-10-29 22:08:19 -0400

    add code formatter for php -in gitignore, add node stuff -on all files, format code to look nicer -npm install prettier and its plugin for php

--- wp-content/themes/fictional-university-theme/footer.php
+++ wp-content/themes/fictional-university-theme/footer.php
@@ -3,7 +3,7 @@
         <div class="group">
             <div class="site-footer__col-one">
                 <h1 class="school-logo-text school-logo-text--alt-color">
-                    <a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a>
+                    <a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a>
                 </h1>
                 <p><a class="site-footer__link" href="#">555.555.5555</a></p>
             </div>
@@ -13,17 +13,17 @@
                     <h3 class="headline headline--small">Explore</h3>
                     <nav class="nav-list">
                         <!-- <ul>
-                            <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
+                            <li><a href="<?php echo site_url(
+                                "/about-us",
+                            ); ?>">About Us</a></li>
                             <li><a href="#">Programs</a></li>
                             <li><a href="#">Events</a></li>
                             <li><a href="#">Campuses</a></li>
                         </ul> -->
 
-                        <?php
-                        wp_nav_menu(array(
-                            'theme_location' => 'footerLocationOne'
-                        ));
-                        ?>
+                        <?php wp_nav_menu([
+                            "theme_location" => "footerLocationOne",
+                        ]); ?>
                     </nav>
                 </div>
 
@@ -32,14 +32,14 @@
                     <nav class="nav-list">
                         <!-- <ul>
                             <li><a href="#">Legal</a></li>
-                            <li><a href="<?php echo site_url('/privacy-policy') ?>">Privacy</a></li>
+                            <li><a href="<?php echo site_url(
+                                "/privacy-policy",
+                            ); ?>">Privacy</a></li>
                             <li><a href="#">Careers</a></li>
                         </ul> -->
-                        <?php
-                        wp_nav_menu(array(
-                            'theme_location' => 'footerLocationTwo'
-                        ));
-                        ?>
+                        <?php wp_nav_menu([
+                            "theme_location" => "footerLocationTwo",
+                        ]); ?>
                     </nav>
                 </div>
             </div>
--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -2,7 +2,9 @@
 get_header(); ?>
 
 <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)"></div>
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri(
+        "/images/ocean.jpg",
+    ); ?>)"></div>
     <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title">Welcome to our blog!</h1>
         <div class="page-banner__intro">
@@ -18,7 +20,9 @@ get_header(); ?>
         <div class="post-item">
             <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
             <div class="metabox">
-                <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', '); ?></p>
+                <p>Posted by <?php the_author_posts_link(); ?> on <?php the_time(
+     "n.j.y",
+ ); ?> in <?php echo get_the_category_list(", "); ?></p>
             </div>
 
             <div class="generic-content">
@@ -26,7 +30,8 @@ get_header(); ?>
                 <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue Reading &raquo;</a></p>
             </div>
         </div>
-    <?php }
+    <?php
+    }
     echo paginate_links();
     ?>
 </div>
