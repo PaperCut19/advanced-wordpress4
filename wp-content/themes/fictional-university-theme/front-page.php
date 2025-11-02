9a2efee3891df90da5b9c4d47da889f14bd741e9
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-11-02 18:39:02 -0500

    update front page date display -use plugin to create custom fields and use the function get_field that the plugin provided to get the 'event date' data to display

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -20,18 +20,34 @@
 $homepageEvents = new WP_Query([
     "posts_per_page" => 3,
     "post_type" => "event",
+    "meta_key" => "event_date",
+    "orderby" => [
+        "meta_value_num" => "ASC",
+        "date" => "ASC",
+    ],
 ]);
 
 while ($homepageEvents->have_posts()) {
-    $homepageEvents->the_post(); ?>
+
+    $homepageEvents->the_post();
+
+    /** @disregard */
+    $eventDateValue = get_field("event_date");
+    if ($eventDateValue) {
+        $eventDate = new DateTime($eventDateValue);
+        $month = $eventDate->format("M");
+        $day = $eventDate->format("d");
+    } else {
+        // Fallback to publication date
+        $month = get_the_date("M");
+        $day = get_the_date("d");
+    }
+    ?>
+
         <div class="event-summary">
                 <a class="event-summary__date t-center" href="#">
-                    <span class="event-summary__month"><?php the_time(
-                        "M",
-                    ); ?></span>
-                    <span class="event-summary__day"><?php the_time(
-                        "d",
-                    ); ?></span>
+                    <span class="event-summary__month"> <?php echo $month; ?></span>
+                    <span class="event-summary__day"><?php echo $day; ?></span>
                 </a>
                 <div class="event-summary__content">
                     <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
