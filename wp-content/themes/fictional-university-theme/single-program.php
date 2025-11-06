116669046e4662e416232b7c61a9fc57e89eb940
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-11-05 19:37:51 -0500

    display the event and program relationship on the frontend -use get_field which comes from the custom field plugin to see if an event has a related program and if it does, display it on the event's page -use a custom query with the correct filters to see if an event is connected to a program, if it is connected, show the event on the program's page -add a stubs file from the custom field plugin so that PHP intelephense extension doesn't mark functions like get_field as errors

--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -30,6 +30,56 @@ while (have_posts()) {
         <div class="generic-content">
             <?php the_content(); ?>
         </div>
+
+        <?php
+        $today = date("Ymd");
+        $homepageEvents = new WP_Query([
+            "posts_per_page" => 3,
+            "post_type" => "event",
+            "meta_key" => "event_date",
+            "orderby" => "meta_value_num",
+            "order" => "ASC",
+            "meta_query" => [
+                [
+                    "key" => "event_date",
+                    "compare" => ">=",
+                    "value" => $today,
+                    "type" => "numeric",
+                ],
+                [
+                    "key" => "related_programs",
+                    "compare" => "LIKE",
+                    "value" => '"' . get_the_ID() . '"',
+                ],
+            ],
+        ]);
+
+        while ($homepageEvents->have_posts()) {
+
+            $homepageEvents->the_post();
+            $eventDateValue = get_field("event_date");
+            $eventDate = new DateTime($eventDateValue);
+            $month = $eventDate->format("M");
+            $day = $eventDate->format("d");
+            ?>
+
+        <div class="event-summary">
+                <a class="event-summary__date t-center" href="#">
+                    <span class="event-summary__month"> <?php echo $month; ?></span>
+                    <span class="event-summary__day"><?php echo $day; ?></span>
+                </a>
+                <div class="event-summary__content">
+                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                    <p><?php if (has_excerpt()) {
+                        echo get_the_excerpt();
+                    } else {
+                        echo wp_trim_words(get_the_content(), 18);
+                    } ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                </div>
+            </div>
+    <?php
+        }
+        ?>
     </div>
 
 <?php
