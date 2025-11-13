de8c776be1e5da06ebddd37f53b2690c6553fb89
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2025-11-13 18:07:28 -0500

    take the user's text to return content -use .getJSON to combine the user's text with a URL so that we can get access to JSON data and then use a callback function to display what we want from that JSON data

--- wp-content/themes/fictional-university-theme/build/index.asset.php
+++ wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => 'af1d523743e20131916c');
+<?php return array('dependencies' => array('jquery'), 'version' => '7895d4680a99db44f138');
