c1ba2c9a598243738f3ccebe2c6f0ae04d4bd02c
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2026-01-13 17:03:03 -0500

    improve user search experience -add code that will display a message if the user searched for something that doesn't exist

--- wp-content/themes/fictional-university-theme/search.php
+++ wp-content/themes/fictional-university-theme/search.php
@@ -8,16 +8,20 @@ pageBanner([
 
 <div class="container container--narrow page-section">
     <?php
-    while (have_posts()) {
+    if (have_posts()) {
+        while (have_posts()) {
         the_post(); 
         
         get_template_part('template-parts/content', get_post_type());
-        ?>
-        
-    <?php
+            
     }
     echo paginate_links();
-    ?>
+    
+    } else {
+        echo '<h2 class="headline headline--small-plus">No results match that search.</h2>';
+    }
+?>
+    
 </div>
 
 <?php get_footer(); ?>
