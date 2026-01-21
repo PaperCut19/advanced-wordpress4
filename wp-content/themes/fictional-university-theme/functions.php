05da7557819f2521dd409dec66215a1964694a98
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2026-01-21 16:35:54 -0500

    improve login experience -study notes on changing the title: We can use add_filter('login_headertitle', callback function). In the callback function, we can use get_bloginfo('name')

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -225,3 +225,10 @@ function ourLoginCSS()
         get_theme_file_uri("/build/index.css"),
     );
 }
+
+add_filter('login_headertitle', 'ourLoginTitle');
+
+function ourLoginTitle()
+{
+    return get_bloginfo('name');
+}
