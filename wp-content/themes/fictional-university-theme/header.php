61f790a503a490ddc5fd9475247c9feef0c3298e
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2026-01-21 17:07:24 -0500

    start work on the 'my notes' feature -add a my notes button to header.php and show it only if the user is logged in -create a php file for the page 'my-notes'

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -13,14 +13,14 @@
             <h1 class="school-logo-text float-left">
                 <a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a>
             </h1>
-            <a href="<?php echo esc_url(site_url('/search')) ?>"  class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></a>
+            <a href="<?php echo esc_url(site_url('/search')) ?>" class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></a>
             <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
             <div class="site-header__menu group">
                 <nav class="main-navigation">
                     <!-- <ul>
                         <li><a href="<?php
-//echo site_url("/about-us",);
-?>">About Us</a></li>
+                                        //echo site_url("/about-us",);
+                                        ?>">About Us</a></li>
                         <li><a href="#">Programs</a></li>
                         <li><a href="#">Events</a></li>
                         <li><a href="#">Campuses</a></li>
@@ -33,6 +33,7 @@
                 </nav>
                 <div class="site-header__util">
                     <?php if (is_user_logged_in()) { ?>
+                        <a href="<?php echo esc_url(site_url('/my-notes')); ?>" class="btn btn--small btn--orange float-left push-right">My Notes</a>
                         <a href="<?php echo wp_logout_url(); ?>" class="btn btn--small btn--dark-orange float-left btn--with-photo">
                             <span class="site-header__avatar"><?php echo get_avatar(get_current_user_id(), 60); ?></span>
                             <span class="btn__text">Log Out</span>
@@ -41,7 +42,7 @@
                         <a href="<?php echo wp_login_url(); ?>" class="btn btn--small btn--orange float-left push-right">Login</a>
                         <a href="<?php echo wp_registration_url(); ?>" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
                     <?php } ?>
-                    
+
                     <a href="<?php echo esc_url(site_url('/search')) ?>" class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i><span style="margin-left: 8px;">"S" to Search</span></a>
                 </div>
             </div>
---
+++ wp-content/themes/fictional-university-theme/page-my-notes.php
@@ -0,0 +1,22 @@
+<?php
+
+get_header();
+
+while (have_posts()) {
+
+    the_post();
+    pageBanner();
+?>
+
+
+
+    <div class="container container--narrow page-section">
+        Custom code will go here.
+    </div>
+
+<?php
+}
+
+get_footer();
+
+?>
