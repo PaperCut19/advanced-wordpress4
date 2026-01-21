1d240051c39586640c6c3dd02477acdbd0425df4
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2026-01-21 18:31:32 -0500

    add 'note' custom post type

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -76,6 +76,22 @@ function university_post_types()
         ],
         "menu_icon" => "dashicons-welcome-learn-more",
     ]);
+
+    // note post type
+    register_post_type("note", [
+        "supports" => ["title", "editor"],
+        "public" => false,
+        'show_ui' => true,
+        "show_in_rest" => true,
+        "labels" => [
+            "name" => "Notes",
+            "add_new_item" => "Add New Note",
+            "edit_item" => "Edit Note",
+            "all_items" => "All Notes",
+            "singular_name" => "Note",
+        ],
+        "menu_icon" => "dashicons-welcome-write-blog",
+    ]);
 }
 
 add_action("init", "university_post_types");
