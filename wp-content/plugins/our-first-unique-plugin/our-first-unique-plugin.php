3ffec9b8d98996c88454595aea736419b00da5b9
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2026-03-23 16:34:50 -0400

    feat: Implement admin settings page for Word Count Plugin
    Refactor the plugin to use an object-oriented structure with the `WordCountAndTimePlugin` class.
    This commit introduces:
    - An admin options page accessible via 'Settings > Word Count'.
    - A basic settings section and field for 'Display Location'.
    - The necessary WordPress API calls (`add_action`, `add_options_page`, `add_settings_section`, `add_settings_field`, `register_setting`) to manage plugin settings.
    - Removal of the previous `the_content` filter and `addToEndOfPost` function, as the plugin's focus shifts to administrative settings.

--- wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
+++ wp-content/plugins/our-first-unique-plugin/our-first-unique-plugin.php
@@ -8,13 +8,48 @@
  Author URI: https://sanchez-labs.com
 */
 
-add_filter('the_content', 'addToEndOfPost');
-
-function addToEndOfPost($content)
+class WordCountAndTimePlugin
 {
-    if (is_single() && is_main_query()) {
-        return $content . '<p>My name is Batman.</p>';
+    function __construct()
+    {
+        add_action('admin_menu', [
+            $this,
+            'adminPage'
+        ]);
+        add_action('admin_init', [$this, 'settings']);
     }
 
-    return $content;
+    function settings()
+    {
+        add_settings_section('wcp_first_section', null, null, 'word-count-settings-page');
+        add_settings_field('wcp_location', 'Display Location', [$this, 'locationHTML'], 'word-count-settings-page', 'wcp_first_section');
+        register_setting('wordcountplugin', 'wcp_location', ['sanitize_callback' => 'sanitize_text_field', 'default' => '0']);
+    }
+
+    function locationHTML()
+    { ?>
+        Hello
+    <?php }
+
+    function adminPage()
+    {
+        add_options_page('Word Count Settings', 'Word Count', 'manage_options', 'word-count-settings-page', [$this, 'ourHTML']);
+    }
+
+    function ourHTML()
+    { ?>
+        <div class="wrap">
+            <h1>Word Count Settings</h1>
+            <form action="options.php" method="POST">
+                <?php
+                do_settings_sections('word-count-settings-page');
+                submit_button();
+                ?>
+            </form>
+        </div>
+<?php }
 }
+
+$wordCountAndTimePlugin = new WordCountAndTimePlugin();
+
+?>
