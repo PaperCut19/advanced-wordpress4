f1d5394962a8422826b3cd9dbf5263ea9ac23fda
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2026-04-03 14:31:48 -0400

    create javascript plugin
    -create plugin folder
    -create javascript plugin file
    -create PHP file that can load the javascript plugin file into the wordpress system

---
+++ wp-content/plugins/are-you-paying-attention/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+    Plugin Name: Are You Paying Attention Quiz
+    Description: Give your readers a multiple choice question.
+    Version: 1.0
+    Author: Cristian Sanchez-Espinosa
+    Author URI: https://sanchez-labs.com
+*/
+
+if (! defined('ABSPATH')) exit;
+
+class AreYouPayingAttention
+{
+    function __construct()
+    {
+        add_action('enqueue_block_editor_assets', [$this, 'adminAssets']);
+    }
+
+    function adminAssets()
+    {
+        wp_enqueue_script('ournewblocktype', plugin_dir_url(__FILE__) . 'test.js', ['wp-blocks']);
+    }
+}
+
+$areYouPayingAttention = new AreYouPayingAttention();
