b4eb58ccb8f5dfa449fcf95fa29c4b41310eb8dd
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2026-04-10 17:30:45 -0400

    feat(quiz): Enhance answer feedback with delayed visuals and click prevention
    Introduces `isCorrectDelayed` state and `useEffect` to manage visual feedback for quiz answers.
    - Incorrect answers reset after 2.6 seconds.
    - Correct answers show a checkmark and disable further clicks after 1 second, while incorrect options fade out.
    This improves the user experience by providing clear, timed feedback and preventing multiple answer attempts after a correct selection.

--- wp-content/plugins/are-you-paying-attention/build/frontend.asset.php
+++ wp-content/plugins/are-you-paying-attention/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '4d3a6169d59ad8c578a7');
+<?php return array('dependencies' => array('react-dom', 'react-jsx-runtime', 'wp-element'), 'version' => '3773c6d3005ba6a2fd44');
--- wp-content/plugins/are-you-paying-attention/build/frontend.js
+++ wp-content/plugins/are-you-paying-attention/build/frontend.js
@@ -148,6 +148,19 @@ divsToUpdate.forEach(function (div) {
 });
 function Quiz(props) {
   const [isCorrect, setIsCorrect] = (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.useState)(undefined);
+  const [isCorrectDelayed, setIsCorrectDelayed] = (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.useState)(undefined);
+  (0,_wordpress_element__WEBPACK_IMPORTED_MODULE_0__.useEffect)(() => {
+    if (isCorrect === false) {
+      setTimeout(() => {
+        setIsCorrect(undefined);
+      }, 2600);
+    }
+    if (isCorrect === true) {
+      setTimeout(() => {
+        setIsCorrectDelayed(true);
+      }, 1000);
+    }
+  }, [isCorrect]);
   function handleAnswer(index) {
     if (index == props.correctAnswer) {
       setIsCorrect(true);
@@ -161,11 +174,30 @@ function Quiz(props) {
       children: props.question
     }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("ul", {
       children: props.answers.map(function (answer, index) {
-        return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("li", {
-          onClick: () => {
+        return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("li", {
+          className: (isCorrectDelayed === true && index == props.correctAnswer ? "no-click" : "") + (isCorrectDelayed === true && index != props.correctAnswer ? "fade-incorrect" : ""),
+          onClick: isCorrect === true ? undefined : () => {
             handleAnswer(index);
           },
-          children: answer
+          children: [isCorrectDelayed === true && index == props.correctAnswer && /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("svg", {
+            xmlns: "http://www.w3.org/2000/svg",
+            width: "20",
+            height: "20",
+            className: "bi bi-check",
+            viewBox: "0 0 16 16",
+            children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("path", {
+              d: "M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z"
+            })
+          }), isCorrectDelayed === true && index != props.correctAnswer && /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("svg", {
+            xmlns: "http://www.w3.org/2000/svg",
+            width: "20",
+            height: "20",
+            className: "bi bi-x",
+            viewBox: "0 0 16 16",
+            children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("path", {
+              d: "M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"
+            })
+          }), answer]
         });
       })
     }), /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsxs)("div", {
