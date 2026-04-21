4d5feda9bd23d5201d3798dfa284af93ffe587a1
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2026-04-21 17:04:10 -0400

    feat: initial block-theme setup
    recycled lots of old code from PHP classic theme in order to recreate the theme using a full-site-editing block architecture
    
    some of the features that are being recycled from the old code:
    - **Build System Integration**: Setup Webpack and Babel for modern JavaScript and Sass compilation, including asset generation and source maps.
    - **Custom Gutenberg Block**: Implemented a new "Banner" block using React, enabling dynamic content display in the block editor.
    - **Custom REST API Endpoints**: Created API endpoints for managing likes on professor posts and a comprehensive search functionality across various post types.
    - **Theme Functionality Enhancements**:
        - Registered multiple navigation menus.
        - Added support for post thumbnails with custom image sizes (professorLandscape, professorPortrait, pageBanner).
        - Modified default WordPress queries for custom post type archives (campus, program, event) to ensure correct ordering and pagination.
        - Integrated Google Maps API and Font Awesome.
    - **User Experience & Security**:
        - Implemented logic to redirect subscriber accounts from the WordPress admin dashboard to the frontend.
        - Hid the admin bar for subscriber roles.
        - Customized the WordPress login screen's branding and styling.
        - Enforced privacy and a note limit for custom 'note' post types.
    - **New Template Parts**: Added dedicated template parts for displaying campus, event, page, post, professor, and program content types.
    - **Asset Management**: Included various image assets for the theme.

---
+++ wp-content/themes/fictional-block-theme/build/banner.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime'), 'version' => '181741c332164ceef384');
---
+++ wp-content/themes/fictional-block-theme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery'), 'version' => '2d8faa99203ede161300');
