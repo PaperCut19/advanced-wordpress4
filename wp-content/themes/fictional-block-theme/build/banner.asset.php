3cfe5cbc8a5a8b19ff7671b2503adda617c988ba
Cristian Sanchez-Espinosa <csesanchez04@icloud.com>
2026-04-22 18:01:40 -0400

    add theme.json to our project in order to configure the styling of our website

--- wp-content/themes/fictional-block-theme/build/banner.asset.php
+++ wp-content/themes/fictional-block-theme/build/banner.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'a2ec388308a1e6afef8b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks'), 'version' => 'e4d4367bb50fa40ca85a');
